50240303a2e9aa2deca24e1736dca2230a29824f
Lia Manning <liapantusa@gmail.com>
2023-04-20 12:04:32 -0400

    Initial Commit

---
+++ GitHub for Pairs/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GitHub for Pairs
+//
+//  Created by Lia Manning on 4/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub for Pairs/GitHub_for_PairsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_for_PairsApp.swift
+//  GitHub for Pairs
+//
+//  Created by Lia Manning on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_for_PairsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
